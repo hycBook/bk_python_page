869ddf8f92d5392ea3c8a1df779089facacea7f9
hycBook <1832044043@qq.com>
2023-08-15 00:53:26 +0000

    Update from https://github.com/hycBook/bk_python/commit/6565506ab4a4b822322d2ec8aef1cf11f8d194c4

--- chapters/res/patch/gitbook-plugin-fontsettings/fontsettings.js
+++ chapters/res/patch/gitbook-plugin-fontsettings/fontsettings.js
@@ -39,6 +39,20 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
              "url(https://s2.loli.net/2022/04/19/JqcAmdxgYIl8kG4.jpg)",
              "url(https://s2.loli.net/2022/04/19/k9RW2etxS6NKVMP.jpg)"];
 
+  var arr = ["url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆0.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆1.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆2.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆3.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆4.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆5.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆6.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆7.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆8.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆9.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆10.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆11.webp)",
+             "url(https://pic.hycbook.com/i/hexo/post_imgs/蕾姆12.webp)"];
+
   function getstyle(sname) {
     for (var i = 0; i < document.styleSheets.length; i++) {
       var rules;
